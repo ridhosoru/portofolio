2016dd8fdda1511954266293e33a521126eea28d
unknown <yohanesridho70@gmail.com>
2025-08-11 15:41:56 +0700

    update size mobile view

--- src/app/maintenance-app/page.js
+++ src/app/maintenance-app/page.js
@@ -49,7 +49,7 @@ export default function MaintenanceApp(){
                     </div>
                 </div>
                 <div className="block lg:hidden">
-                    <div className="w-full h-40 ">
+                    <div className="w-full h-40 mt-12">
                         <div className="text-[#FA812F] text-2xl font-inter font-bold flex mt-2 justify-center items-center">Maintenance App</div>
                         <div className="text-white mt-10 ml-2">Maintenance App vesion 1.0 has release!!!!</div>
                         <a href="https://github.com/ridhosoru/MaintenaceApp/archive/refs/heads/main.zip" className="text-[#FA812F] ml-2">Download Here</a>
