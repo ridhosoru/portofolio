f4a67d42c278bc08b7620376e6c9bb6a1b410da6
unknown <yohanesridho70@gmail.com>
2025-08-11 15:32:52 +0700

    update navbar for mtcapp in mobile view

--- src/app/maintenance-app/page.js
+++ src/app/maintenance-app/page.js
@@ -37,7 +37,22 @@ export default function MaintenanceApp(){
                     <div className="w-full h-40 ">
                         <div className="text-[#FA812F] text-2xl font-inter font-bold flex mt-2 justify-center items-center">Maintenance App</div>
                         <div className="text-white mt-10 ml-2">Maintenance App vesion 1.0 has release!!!!</div>
-                        <a href="#" className="text-[#FA812F] ml-2">Download Here</a>
+                        <a href="https://github.com/ridhosoru/MaintenaceApp/archive/refs/heads/main.zip" className="text-[#FA812F] ml-2">Download Here</a>
+                        <div className="w-[98%] h-1 bg-gray-500 rounded-xl mt-5 flex mx-auto"></div>
+                    </div>
+                    <div className="prose prose-invert w-10/12 mx-auto min-h-screen text-white">
+                        <ReactMarkdown 
+                        remarkPlugins={[remarkGfm]}
+                        rehypePlugins={[rehypeRaw]}>
+                            {readme}
+                        </ReactMarkdown>
+                    </div>
+                </div>
+                <div className="block lg:hidden">
+                    <div className="w-full h-40 ">
+                        <div className="text-[#FA812F] text-2xl font-inter font-bold flex mt-2 justify-center items-center">Maintenance App</div>
+                        <div className="text-white mt-10 ml-2">Maintenance App vesion 1.0 has release!!!!</div>
+                        <a href="https://github.com/ridhosoru/MaintenaceApp/archive/refs/heads/main.zip" className="text-[#FA812F] ml-2">Download Here</a>
                         <div className="w-[98%] h-1 bg-gray-500 rounded-xl mt-5 flex mx-auto"></div>
                     </div>
                     <div className="prose prose-invert w-10/12 mx-auto min-h-screen text-white">
